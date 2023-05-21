89ff169e030f57cd681ee549210823dda0abfa88
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2023-05-21 17:22:50 +0545

    configs

--- variables.ts
+++ variables.ts
@@ -1,4 +1,23 @@
 const API_BASE_URL = process.env.NEXT_APP_API_BASE_URL;
-const PORTAL_MAIN_CONTAINER_ID = "portal-main-container";
+const PRODUCTION_URL = process.env.NEXT_APP_APP_BASE_URL;
+const PORTAL_MAIN_CONTAINER_ID = 'portal-main-container';
 
-export { API_BASE_URL, PORTAL_MAIN_CONTAINER_ID };
+/**
+ * SEO constants
+ */
+const SEO_TITLE = 'Next Blogging | Scholarsspace';
+const SEO_DESCRIPTION = 'Next Blogging | Scholarsspace - A Next.js Blogging Template with Chakra UI';
+const SEO_KEYWORDS = 'nextjs, next, react, chakra, chakra ui, blog, blogging, template';
+const SEO_OG_IMAGE = `${PRODUCTION_URL}/favicon.png`;
+const SEO_TWITTER_IMAGE = `${PRODUCTION_URL}/favicon.png`;
+
+export {
+    API_BASE_URL,
+    PRODUCTION_URL,
+    PORTAL_MAIN_CONTAINER_ID,
+    SEO_TITLE,
+    SEO_DESCRIPTION,
+    SEO_KEYWORDS,
+    SEO_OG_IMAGE,
+    SEO_TWITTER_IMAGE,
+};
