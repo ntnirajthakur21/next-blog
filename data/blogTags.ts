663b2437c09ae2253b3f831e22e351e7fcce8721
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2023-05-24 01:26:55 +0545

    static data

---
+++ data/blogTags.ts
@@ -0,0 +1,39 @@
+const BlogTags: {
+    name: string;
+    color: string;
+}[] = [
+    {
+        name: 'All',
+        color: 'gray',
+    },
+    {
+        name: 'React',
+        color: 'blue',
+    },
+    {
+        name: 'NextJS',
+        color: 'teal',
+    },
+    {
+        name: 'NodeJS',
+        color: 'green',
+    },
+    {
+        name: 'MongoDB',
+        color: 'green',
+    },
+    {
+        name: 'ExpressJS',
+        color: 'green',
+    },
+    {
+        name: 'JavaScript',
+        color: 'yellow',
+    },
+    {
+        name: 'TypeScript',
+        color: 'yellow',
+    },
+];
+
+export default BlogTags;
